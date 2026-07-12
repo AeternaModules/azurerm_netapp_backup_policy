8222acc5da2b24ff2f0676e92fc8df1188675d6b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "netapp_backup_policies" {
-  description = "All netapp_backup_policy resources"
-  value       = azurerm_netapp_backup_policy.netapp_backup_policies
-}
 output "netapp_backup_policies_account_name" {
-  description = "List of account_name values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.account_name]
+  description = "Map of account_name values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.account_name }
 }
 output "netapp_backup_policies_daily_backups_to_keep" {
-  description = "List of daily_backups_to_keep values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.daily_backups_to_keep]
+  description = "Map of daily_backups_to_keep values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.daily_backups_to_keep }
 }
 output "netapp_backup_policies_enabled" {
-  description = "List of enabled values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.enabled]
+  description = "Map of enabled values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.enabled }
 }
 output "netapp_backup_policies_location" {
-  description = "List of location values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.location]
+  description = "Map of location values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.location }
 }
 output "netapp_backup_policies_monthly_backups_to_keep" {
-  description = "List of monthly_backups_to_keep values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.monthly_backups_to_keep]
+  description = "Map of monthly_backups_to_keep values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.monthly_backups_to_keep }
 }
 output "netapp_backup_policies_name" {
-  description = "List of name values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.name]
+  description = "Map of name values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.name }
 }
 output "netapp_backup_policies_resource_group_name" {
-  description = "List of resource_group_name values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.resource_group_name]
+  description = "Map of resource_group_name values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.resource_group_name }
 }
 output "netapp_backup_policies_tags" {
-  description = "List of tags values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.tags]
+  description = "Map of tags values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.tags }
 }
 output "netapp_backup_policies_weekly_backups_to_keep" {
-  description = "List of weekly_backups_to_keep values across all netapp_backup_policies"
-  value       = [for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : v.weekly_backups_to_keep]
+  description = "Map of weekly_backups_to_keep values across all netapp_backup_policies, keyed the same as var.netapp_backup_policies"
+  value       = { for k, v in azurerm_netapp_backup_policy.netapp_backup_policies : k => v.weekly_backups_to_keep }
 }
 
